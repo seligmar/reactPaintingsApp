97c9eb1abda417436612aa56902c006059d24758
github username <github email address>
2021-04-05 14:46:56 +0100

    began passing paintings as props

--- react-paintings-app/src/App.js
+++ react-paintings-app/src/App.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import ReactDom from 'react-dom'
 import 'semantic-ui-css/semantic.min.css'
-
-import './App.css'
 import Navbar from './Navbar'
+import allPaintings from './data/data'
+import Paintings from './Paintings'
 
 function App () {
   return (
     <div className='App'>
       <Navbar
-        color='red'
+        color='blue'
         icon='react'
         title='My painting App'
         subtitle="Gotta Catch 'em All"
       />
+      <Paintings paintings={allPaintings} />
     </div>
   )
 }
---
+++ react-paintings-app/src/Paintings.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Paintings = props => {
+  console.log('props?', props)
+  return 'its working?'
+}
+
+export default Paintings
